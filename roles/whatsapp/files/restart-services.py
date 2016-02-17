4a37d71139088bd655bf8e700a1ffbb7392812eb
jmwai <smartemwa@gmail.com>
2016-02-17 14:11:31 +0300

    Created python script to find and restart all ongair agents in server

---
+++ roles/whatsapp/files/restart-services.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import re
+
+conf = os.path.dirname("/etc/init/")
+
+ongair_service_format = r'^ongair-(.+)\.conf$'
+ongair_confs = list(
+    filter((lambda file: re.search(ongair_service_format, file)),
+           os.listdir(conf)))
+
+services = []
+for item in ongair_confs:
+    match = re.search(ongair_service_format, item)
+    if match:
+        service = match.group(1)
+        services.append(service)
+        os.system("sudo service restart %s" % (service))
+    else:
+        print("Not an ongair service")
+
+print(services)
