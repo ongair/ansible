8b79c299f1bacea8f92a55566d1931fb50156c6e
jmwai <smartemwa@gmail.com>
2016-03-14 14:08:16 +0300

    Added the python play module to add free trial numbers dynamically

--- play.py
+++ play.py
@@ -15,13 +15,14 @@ from ansible.inventory import Inventory
 from ansible import callbacks
 from ansible import utils
 
+app = Flask(__name__)
 
 inventory = """
 deploy_user: {{deploy_user}}
 agents:
-    - { account_number: {{number}}, agent_name: {{agent_name}} }
-
+       - { account_number: {{number}}, agent_name: {{agent_name}} }
 """
+
 
 # Boilerplace callbacks for stdout/stderr and log output
 utils.VERBOSITY = 0
@@ -30,37 +31,48 @@ stats = callbacks.AggregateStats()
 runner_cb = callbacks.PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY)
 
 
-@app.route('add-number/<number>')
-def trial():
-	inventory_template = jinja2.Template(inventory)
-	rendered_inventory = inventory_template.render({
+# @app.route('add-number/<number>')
+@app.route('/trial/<number>')
+def trial(number):
+    # number = number
+    inventory_template = jinja2.Template(inventory)
+    rendered_inventory = inventory_template.render({
     'deploy_user': 'ubuntu',
     'number': number,
     'agent_name': 'ongair-%s' % (number)
     })
-
-    host = NamedTemporaryFile(delete=False)
-    host.write(rendered_inventory)
-    host.close()
+    print(rendered_inventory)
+    file_path = os.path.join(os.path.abspath('group_vars'), 'ongair-ec2')
+    print(file_path)
+    try:
+        with open(file_path, "w+") as f:
+            f.write(rendered_inventory)
+    except IOError, e:
+        print("cant write to file")
+    # hosts = NamedTemporaryFile(delete=False)
+    # hosts.write(rendered_inventory)
+    # host.name = 'ongair-ec2'
+    # hosts.close()
     pb = PlayBook(
-    playbook='deploy.yml',
-    host_list='ec2-test',     # Our hosts, the rendered inventory file
-    remote_user='ubuntu',
-    callbacks=playbook_cb,
-    runner_callbacks=runner_cb,
-    stats=stats,
-    private_key_file='/home/james/.ssh/ongair-whatsapp-key.pem'
+        playbook='add-to-trial.yml',
+        # host_list=hosts.name,
+        # inventory=hosts.name,     # Our hosts, the rendered inventory file
+        remote_user='ubuntu',
+        callbacks=playbook_cb,
+        runner_callbacks=runner_cb,
+        stats=stats,
+        vault_password='whts@99Ongair-3#ncrypt!2@16',                                    
+        private_key_file='~/.ssh/ongair-whatsapp-key.pem'
     )
     results = pb.run()
     # Ensure on_stats callback is called
     # for callback modules
     playbook_cb.on_stats(pb.stats)
-    os.remove(hosts.name)
     print results
 
 
 
-    return jsonify('details')
+    return jsonify(results)
 
 
 if __name__ == '__main__':
