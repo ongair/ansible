e144ceff6754795e8fb5c00383ad6be29c8395e7
jmwai <smartemwa@gmail.com>
2016-03-17 10:54:13 +0300

    Created a standalone ansible module.

--- ongair/play.py
+++ ongair/play.py
@@ -21,8 +21,7 @@ from ansible import utils
 
 app = Flask(__name__)
 
-inventory = """
-deploy_user: {{deploy_user}}
+inventory = """deploy_user: {{deploy_user}}
 agents:
        - { account_number: {{number}}, agent_name: {{agent_name}} }
 """
@@ -43,6 +42,7 @@ def index():
 
 @app.route('/trial')
 def trial():
+    start = time.time()
     if not 'number' in request.args:
         message = 'Please send the request with a trial this format: /trial?number=<number>'
         data = {
@@ -65,7 +65,8 @@ def trial():
     })
     file_path = os.path.join(os.path.abspath('..'), 'group_vars/ongair-ec2')
     playbook_path = os.path.join(os.path.abspath('..'), 'add-to-trial.yml')
-    print(playbook_path)
+    inventory_path = os.path.join(os.path.abspath('..'), 'trial')
+    print(inventory_path)
     try:
         with open(file_path, "w+") as f:
             f.write(rendered_inventory)
@@ -78,24 +79,28 @@ def trial():
         callbacks=playbook_cb,
         runner_callbacks=runner_cb,
         stats=stats,
+        # inventory=Inventory(['inventory_path']),
+        # host_list='inventory_path',
         # vault_password=os.environ['ongairvaultpassword'],
         vault_password='whts@99Ongair-3#ncrypt!2@16'
     )
     try:
-        start = time.time()
+        
         results = pb.run()
-        end = time.time()
+        
     except Exception, e:
         print e
 
     # Ensure on_stats callback is called
     # for callback modules
     playbook_cb.on_stats(pb.stats)
+    end = time.time()
+    time_taken = end - start
     data = {
         "message": "successfully added %s to trial" % (number),
         "status": 200,
         "data": results,
-        "time_taken": end-start
+        "time_taken": round(time_taken, 2)
     }
     js = json.dumps(data)
     resp = Response(js, status=200, mimetype='application/json')
